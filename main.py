fd4a813c0519fab430fedc4a3d046c6425302354
Maxim Kohanov <M.Kohanov.inf23@stud.bbbaden.ch>
2026-06-18 09:46:41 +0200

    Added persistant data base in txt file in project folder, improved code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from telethon import events
 from telethon.tl.types import User
 import datetime
 import asyncio
+from pathlib import Path
 
 load_dotenv(override=True)
 
@@ -14,16 +15,62 @@ API_HASH = os.environ.get("API_HASH")
 
 #CUSTOM - Add your keywords here, you can also change the session name which is purely cosmetic
 session_name = "Developement"
-keywords = [
-    ""
-]
+
 
 client = telethon.TelegramClient(session_name, API_ID, API_HASH)
 
-def enterKeywords():
-    keywords = input("Enter keywords seperated by commas (,): ").split(",")
+def logResult(result):
+    date = getDateTime()
+    log = Path("log.txt")
+    try:
+        with open(log, "a", encoding="utf-8") as l:
+            l.write(f"\n{date}\n{result}\n")
+    except Exception as e:
+         print(f"ERROR: {e}")
+
+#TODO Add function to keep out duplicates
+def enterKeywords(passedValue=None):
+    keywords = Path("keywords.txt")
+    if passedValue is None:
+        userInput = input("Enter keywords seperated by commas (,): ").strip().lower().split(",")
+    else:
+        keyfile = Path("keywords.txt")
+        with open(keyfile, "w", encoding="utf-8") as k: 
+            k.write("")
+        userInput = passedValue
+    with open (keywords, "a+", encoding="utf-8") as k:
+        keywordsFile = k.read()
+        for entry in userInput:
+            if entry.strip() not in keywordsFile:
+                k.write(f"{entry.strip()}\n")
+
+def readKeywords():
+    keywords = []
+    keyfile = Path("keywords.txt")
+    with open(keyfile, "r", encoding="utf-8") as k: 
+        for line in k:
+            if line.strip():
+                keywords.append(line.strip())
     return keywords
 
+def editKeyfile():
+    choice = input("Delete all [1], or certain keywords [2]?: ").strip()
+    if choice.strip() == "1":
+        keyfile = Path("keywords.txt")
+        with open(keyfile, "w", encoding="utf-8") as k: 
+            k.write("")
+        enterKeywords()
+    elif choice.strip() == "2":
+        
+        #TODO fix comparison logic
+        keywordsToDelete = input("Enter the keywords to delete seperated by a comma (,): ").lower().split(",")
+        oldKeywords = readKeywords()
+        newKeywords = []
+        for entry in oldKeywords:
+            if entry not in keywordsToDelete:
+                    newKeywords.append(entry)
+        enterKeywords(newKeywords)
+                
 def getDateTime():
     Date = datetime.datetime.now()
     return Date
@@ -50,7 +97,7 @@ def histroyScanUserInput():
             print("Error", e)
     return desiredDate
 
-async def searchPastHistory(targetDate, keywords):
+async def searchPastHistory(targetDate):
     #as of right now, if a message contains more than 1 keyword it will send a message for each keyword, this will send the message only once using the message id
     alreadyScannedResults = set()
     foundResults = 0
@@ -59,8 +106,10 @@ async def searchPastHistory(targetDate, keywords):
       
         if isinstance(chat.entity, User):
             continue
+        keywords = readKeywords()
         async for msg in client.iter_messages(chatIdentifier, limit=10000):
             try:
+                 
                  name = chat.title
                  text = (msg.raw_text or "").lower()
                  textID = msg.id
@@ -76,8 +125,10 @@ async def searchPastHistory(targetDate, keywords):
                         alreadyScannedResults.add(textID)
                         print("Connection found!")
                         foundResults = foundResults + 1
-                        await client.send_message("me", f"Connection found!\nKeyword: {entries}\nMessage origin: {name}\nMessage ID: {textID}\n Message time: {textDate}\nMessage: {text}\n")
-                        await asyncio.sleep(2)
+                        result = (f"Connection found!\nKeyword: {entries}\nMessage origin: {name}\nMessage ID: {textID}\n Message time: {textDate}\nMessage: {text}\n")
+                        print(result)
+                        await asyncio.sleep(1)
+                        logResult(result)
                         
             except Exception as e:
                 print(f"\nERROR: {e}\n")
@@ -89,6 +140,8 @@ async def handler(event):
          return
     
     try:
+        
+        keywords = readKeywords()
         channel = await event.get_chat()
         name = channel.title
         text = (event.raw_text or "").lower()
@@ -96,8 +149,10 @@ async def handler(event):
         textDate = event.date
 
         for entries in keywords:
-            if entries.lower() in text:
-                    await client.send_message("me", f"Connection found!\nKeyword: {entries}\nMessage origin: {name}\nMessage ID: {textID}\n Message time: {textDate}\nMessage: {text}")
+            if entries in text:
+                    result = (f"Connection found!\nKeyword: {entries}\nMessage origin: {name}\nMessage ID: {textID}\n Message time: {textDate}\nMessage: {text}\n")
+                    await client.send_message("me", f"{result}")
+                    logResult(result)
             
        
     except Exception as e:
@@ -105,7 +160,19 @@ async def handler(event):
      
 async def main():
     print(f"Connection successfull. Session name: {session_name}")
-    keywords = enterKeywords()
+    if not Path("keywords.txt").exists() or Path("keywords.txt").stat().st_size == 0:
+        enterKeywords()
+    else: 
+        adjustKeywords = input("Do you want to adjust or delete keywords?: [y][n]")
+        if adjustKeywords.lower().strip() == "y" or adjustKeywords.lower().strip() == "yes":
+            keyfileEditMode = input("Add [1] or delete [2] keywords?").strip()
+            if keyfileEditMode == "1":
+                enterKeywords()
+            elif keyfileEditMode == "2":
+                editKeyfile()
+
+
+
     historyScan = doesUserWantToScanHistory()
     if historyScan:
         desiredDate = histroyScanUserInput()
@@ -113,7 +180,7 @@ async def main():
     await client.send_message("me", "Connection successfull.")
     print("Connection succesfull.")
     if historyScan:
-         await searchPastHistory(desiredDate, keywords)
+         await searchPastHistory(desiredDate)
          print("History scan completed, starting live monitoring.\n")
          await client.send_message("me", "History scan completed, starting live monitoring.")
     await client.run_until_disconnected()
